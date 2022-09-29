5ce69e42b9563f7d1c74533a6ca99a4a6732aa88
Oliulla <razasm29@gmail.com>
2022-09-29 14:42:17 +0600

    added states props blog

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -36,6 +36,70 @@ const Blog = () => {
                         their components in browser through virtual DOM.
                     </p>
                 </div>
+                <div className="blog">
+                    <h3>Differences Between Props And States In React</h3>
+                    <i>Ans:</i>
+                    <div className='props-sates'>
+                        <div>
+                            <h3>States</h3>
+                            <ol className='order-list'>
+                                <li>
+                                    State changes can be asynchronous.
+                                </li>
+                                <li>
+                                    Stats are mutable.
+                                </li>
+                                <li>
+                                    State holds information about the components.
+                                </li>
+                                <li>
+                                    State cannot be accessed by child components.
+                                </li>
+                                <li>
+                                    States can be used for rendering dynamic changes with the component.
+                                </li>
+                                <li>
+                                    Stateless components cannot have State
+                                </li>
+                                <li>
+                                    State cannot make components reusable.
+                                </li>
+                                <li>
+                                    The State is internal and controlled by the React Component itself.
+                                </li>
+                            </ol>
+                        </div>
+                        <div>
+                            <h3>Props</h3>
+                            <ol className='order-list'>
+                                <li>
+                                    Props are read-only.
+                                </li>
+                                <li>
+                                    Props are immutable.
+                                </li>
+                                <li>
+                                    Props allow you to pass data from one component to other components as an argument.
+                                </li>
+                                <li>
+                                    Props can be accessed by the child component.
+                                </li>
+                                <li>
+                                    Props are used to communicate between components.
+                                </li>
+                                <li>
+                                    Stateless component can have Props.
+                                </li>
+                                <li>
+                                    Props make components reusable.
+                                </li>
+                                <li>
+                                    Props are external and controlled by whatever renders the component.
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
