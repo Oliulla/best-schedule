1c5675126d4df628ee3b7cc8dc95df6ae54873b8
Oliulla <razasm29@gmail.com>
2022-09-29 12:39:14 +0600

    added responsive breakpoint

--- src/components/Main/cartcomponents/ActivityDetails/ActivityDetails.js
+++ src/components/Main/cartcomponents/ActivityDetails/ActivityDetails.js
@@ -6,7 +6,7 @@ const ActivityDetails = ({activityTime}) => {
         <div className='activity-details'>
             <h4>Activity Details</h4>
             <div className='activity-detail-container'>
-                <h5>Activity Time: </h5>
+                <h5>Activity Time </h5>
                 <p>{activityTime}<small>hrs</small></p>
             </div>
         </div>
